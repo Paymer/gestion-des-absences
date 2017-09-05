2e9b656e4439d49eddb45d66a08a10b85348f8a1
Paula Aymerich <p.aymerich.f@gmail.com>
2017-09-05 13:32:08 +0200

    menuEmploye-finished

--- front/app/menuEmploye/menuEmploye.controller.js
+++ front/app/menuEmploye/menuEmploye.controller.js
@@ -1,13 +1,13 @@
 export default class MenuEmployeCtrl{
-    constructor(logService){
-        
-        this.logService = logService
+    constructor(){
+        //logService
+        //this.logService = logService
         this.logo = {title: "Logo", url: "#"}
     }
     
     getConnectionStatus(){
-        return this.logService.isConnected()
+        //return this.logService.isConnected()
     }
 }
 
-MenuEmployeCtrl.$inject = ['logService']
+//MenuEmployeCtrl.$inject = ['logService']
