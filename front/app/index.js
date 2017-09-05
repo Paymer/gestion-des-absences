c1553c46ca004690d38a4b2ffc46361ff9643e81
Paula Aymerich <p.aymerich.f@gmail.com>
2017-09-05 14:01:56 +0200

    USGDA002 - menu administrateur #4

--- front/app/index.js
+++ front/app/index.js
@@ -5,11 +5,12 @@ import { route } from './app.route';
 import { AccueilComponent } from './accueil/accueil.component';
 
 
-
+//Modules
 import menuEmployeModule from './menuEmploye/menuEmploye.module';
+import menuAdminModule from './menuAdministrateur/menuAdministrateur.module';
 
 
-angular.module('app', [RouteModule, menuEmployeModule.name])
+angular.module('app', [RouteModule, menuEmployeModule.name, menuAdminModule.name])
     .value('apiUrl', API_URL)
     .component('accueil', AccueilComponent)
     .config(route);
