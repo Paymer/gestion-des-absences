19d46a2458e8c96b742364dc846bfaa9f260754a
Rossi Oddet <rossi.oddet@dev-institut.fr>
2017-09-05 09:44:27 +0200

    Revert "injection code interdit js pour analyse sonar"
    This reverts commit 10e6db34e79877775b4d7c928394a6d07c37dd91.

--- front/app/index.js
+++ front/app/index.js
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { route } from './app.route';
 import { AccueilComponent } from './accueil/accueil.component';
 
-// code interdit !
-alert("oops")
 angular.module('app', [RouteModule])
 .value( 'apiUrl', API_URL)
 .component('accueil', AccueilComponent)
