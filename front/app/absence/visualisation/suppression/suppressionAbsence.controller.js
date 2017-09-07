61ae92ef7e91078cf412f29071058ee89cab2361
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-09-07 15:12:16 +0200

    ajout test visualisation absence #5

--- front/app/absence/visualisation/suppression/suppressionAbsence.controller.js
+++ front/app/absence/visualisation/suppression/suppressionAbsence.controller.js
@@ -1,10 +1,14 @@
 
 export default class SuppressionAbsenceController {
-    constructor($uibModalInstance) {
+    constructor($uibModalInstance, suppressionAbsenceService, absenceId) {
         this.$uibModalInstance = $uibModalInstance;
+        this.suppressionAbsenceService = suppressionAbsenceService;
+        this.absenceId = absenceId;
     }
 
     valider() {
+        console.log("A faire : supprimer l'absence d'id :", this.absenceId);
+        this.suppressionAbsenceService.supprimerAbsence(this.absenceId);
         this.$uibModalInstance.close("Suppression de l'absence");
     }
 
--- front/app/absence/visualisation/visualisationAbsence.controller.js
+++ front/app/absence/visualisation/visualisationAbsence.controller.js
@@ -18,7 +18,7 @@ export default class VisualisationAbsenceController {
 
     // Partie suppression de l'absence
 
-    supprimerAbsence() {
-        this.visualisationAbsenceService.supprimerAbsence();
+    supprimerAbsence(absenceId) {
+        this.visualisationAbsenceService.supprimerAbsence(absenceId);
     }
 }
