1e8611d08b321d1535017814b0203dee52f21d04
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-09-14 15:08:11 +0200

    ajout du choix du rapport dans les vues synthétiques #11

--- front/app/index.js
+++ front/app/index.js
@@ -23,6 +23,7 @@ import planningAbsenceComponent from './absence/planning/planningAbsence.compone
 import visualisationFerieComponent from "./ferie/visualisation/visualisationFerie.component"
 import creationFerieComponent from './ferie/creation/creationFerie.component'
 import vueDepJourCollabComponent from "./vueSynthetiques/vueDepartementJourCollaborateur/vueDepJourCollab.component"
+import rapportComponent from "./vueSynthetiques/choixRapports/rapport.component"
 
 // Services
 import apiUrls from "./utils/apiUrls.service"
@@ -72,6 +73,7 @@ angular.module('app', [RouteModule, ngResource, menuModule.name, uiBootstrap, bo
     .component("visualisationFerieComponent", visualisationFerieComponent)
     .component("vueDepJourCollabComponent", vueDepJourCollabComponent)
     .component('creationFerieComponent',creationFerieComponent)
+    .component('rapportComponent', rapportComponent)
 
     //manage connections and routes
     .config(route)
